e3a1f77e3c73e486a3693fdefc606e52b85de7a2
reo-dev <dbckd22@gmail.com>
2021-03-16 17:33:05 +0900

    Make pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Link
+} from "react-router-dom";
+import MainPage from './pages/MainPage'
+import LoginPage from './pages/LoginPage'
+import SignupPage from './pages/SignupPage'
+import MyPage from './pages/MyPage'
 
 function App() {
   return (
-    <div className="App">
-      <header className=   "App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div>
+      <Switch>
+      <Route  path="/" component={MainPage}/>
+      <Route  path="/login" component={LoginPage}/>
+      <Route  path="/signup" component={SignupPage}/>
+      <Route  path="/mypage" component={MyPage}/>
+      </Switch>
     </div>
+  </Router>
   );
 }
 
