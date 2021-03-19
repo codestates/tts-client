752f23b2396774db2cc71d9da344a03dd77e6282
ohveloper <ohveloper@gmail.com>
2021-03-19 17:20:38 +0900

    css with clock

--- src/components/ClockAndRecordBox.js
+++ src/components/ClockAndRecordBox.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Clock from "./Clock";
 import OneRecordBox from "./OneRecordBox";
+import Grade from "./Grade";
 
 function ClockAndRecordBox() {
   return (
     <>
-      ClockAndRecordBox
+      <Grade />
       <Clock />
       <OneRecordBox />
     </>
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -2,13 +2,18 @@ import React from "react";
 import NavBar from "../components/NavBar";
 import Clock from "../components/Clock";
 import OneRecordBox from "../components/OneRecordBox";
+import Grade from "../components/Grade";
+import "../componentsCss/MainPage.css";
 
 function MainPage() {
   return (
     <div>
-      <NavBar />
-      <Clock />
-      <OneRecordBox />
+      <div className="allInBox">
+        <NavBar />
+        <Grade />
+        <Clock />
+        <OneRecordBox />
+      </div>
     </div>
   );
 }
