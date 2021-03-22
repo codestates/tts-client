0ee8f59389e03d12be0d453f7307012c2629c871
ohveloper <ohveloper@gmail.com>
2021-03-22 17:51:00 +0900

    done

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -3,25 +3,16 @@ export const SET_ISLOGIN = "SET_ISLOGIN";
 export const SET_LOGOUT = "SET_LOGOUT";
 const axios = require("axios");
 
-<<<<<<< HEAD
 export function setUserInfo() {
-  const response = axios.get("http://localhost:5000/user/userinfo", { withCredentials: true }).then((res) => res.data);
-=======
-export function setUserInfo (){
-    return axios.get('https://localhost:5000/user/userinfo',{accept:'application/json',withCredentials:true}).then(res=>res.data)
-    .then(data=>{
-        return {
-            type: SET_USERINFO,
-            payload: data.data
-        };
-    })
-};
->>>>>>> 5e02fed522588dd5636555b0d37e67a51242caab
-
-  return {
-    type: SET_USERINFO,
-    payload: response.data,
-  };
+  return axios
+    .get("https://localhost:5000/user/userinfo", { accept: "application/json", withCredentials: true })
+    .then((res) => res.data)
+    .then((data) => {
+      return {
+        type: SET_USERINFO,
+        payload: data.data,
+      };
+    });
 }
 
 export function setIsLogin() {
