935f6477ae054a2bc047b8333a7c976f87bed20c
ohveloper <ohveloper@gmail.com>
2021-03-23 22:07:26 +0900

    Feat#20 make userinfo in mypage : chage userinfo & check grade & how to get other grade

--- src/App.js
+++ src/App.js
@@ -1,78 +1,82 @@
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-import {useDispatch, useSelector} from "react-redux";
-import {setIsLogin, setUserInfo} from './actions/userAction'
-import { setIsLoading } from './actions/LoadingAction'
-import axios from 'axios'
-import React,{ useEffect } from "react";
-import MainPage from './pages/MainPage'
-import LoginPage from './pages/LoginPage'
-import ModalPage from './pages/ModalPage'
-import MyPage from './pages/MyPage'
-import WelcomePage from './pages/WelcomePage'
-import Loading from './pages/Loading'
-
- 
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { setIsLogin, setUserInfo } from "./actions/userAction";
+import { setIsLoading } from "./actions/LoadingAction";
+import axios from "axios";
+import React, { useEffect } from "react";
+import MainPage from "./pages/MainPage";
+import LoginPage from "./pages/LoginPage";
+import ModalPage from "./pages/ModalPage";
+import MyPage from "./pages/MyPage";
+import WelcomePage from "./pages/WelcomePage";
+import Loading from "./pages/Loading";
 
 function App() {
-  const dispatch = useDispatch()
-  const { isLoading } = useSelector(state => state.recordReducer)
+  const dispatch = useDispatch();
+  const { isLoading } = useSelector((state) => state.recordReducer);
 
-  const getAccessToken = (authorizationCode)=>{
+  const getAccessToken = (authorizationCode) => {
     dispatch(setIsLoading(true));
-    axios.post('https://localhost:5000/main/oauth/accesstoken', { authorizationCode: authorizationCode,accept:'application/json',withCredentials:true})
-    .then(res=> {
-      return res.data.data.accessToken
-    }).then(accessToken => {
-      getUserInfo(accessToken);
-    })
-  }
-
-  const getUserInfo = (accessToken)=>{
-    axios.get('https://api.github.com/user',{headers:{authorization:`token ${accessToken}`, accept: 'application/json'}})
-    .then(res=>{
-      const { login, id, name } = res.data;
-      const param = {email: `${login}@github.com`, password: id, userName: name?name:login};
-      axios.post('https://localhost:5000/main/signup', param, {accept: 'application/json'})
-      .catch(e => console.log('이미 가입된 이메일'))
-      .finally(e => {
-        axios.post('https://localhost:5000/main/login', param, {accept: 'application/json',withCredentials:true})
-        .then(res => {
-          dispatch(setIsLogin());
-          dispatch(setUserInfo());
-        }).then(d => {dispatch(setIsLoading(false));})
+    axios
+      .post("https://localhost:5000/main/oauth/accesstoken", { authorizationCode: authorizationCode, accept: "application/json", withCredentials: true })
+      .then((res) => {
+        return res.data.data.accessToken;
       })
-    }).catch(e => {
-      alert('OAuth 요청에 실패하였습니다.');
-      dispatch(setIsLoading(false));
-    })
-  }
+      .then((accessToken) => {
+        getUserInfo(accessToken);
+      });
+  };
+
+  const getUserInfo = (accessToken) => {
+    axios
+      .get("https://api.github.com/user", { headers: { authorization: `token ${accessToken}`, accept: "application/json" } })
+      .then((res) => {
+        const { login, id, name } = res.data;
+        const param = { email: `${login}@github.com`, password: id, userName: name ? name : login };
+        axios
+          .post("https://localhost:5000/main/signup", param, { accept: "application/json" })
+          .catch((e) => console.log("이미 가입된 이메일"))
+          .finally((e) => {
+            axios
+              .post("https://localhost:5000/main/login", param, { accept: "application/json", withCredentials: true })
+              .then((res) => {
+                dispatch(setIsLogin());
+                dispatch(setUserInfo());
+              })
+              .then((d) => {
+                dispatch(setIsLoading(false));
+              });
+          });
+      })
+      .catch((e) => {
+        alert("OAuth 요청에 실패하였습니다.");
+        dispatch(setIsLoading(false));
+      });
+  };
 
   useEffect(() => {
-    const url = new URL(window.location.href)
-    const authorizationCode = url.searchParams.get('code')
+    const url = new URL(window.location.href);
+    const authorizationCode = url.searchParams.get("code");
     if (authorizationCode) {
-      getAccessToken(authorizationCode)
+      getAccessToken(authorizationCode);
     }
   }, []);
 
-  return (
-    isLoading ? <Loading /> :
+  return isLoading ? (
+    <Loading />
+  ) : (
     <Router>
-    <div>
-      <Switch>
-      <Route  exact path="/" component={LoginPage}/>
-      <Route  path="/main" component={MainPage}/>
-      <Route  path="/welcome" component={WelcomePage}/>
-      <Route  path="/signup" component={ModalPage}/>
-      <Route  path="/mypage" component={MyPage}/>
-      </Switch>
-    </div>
-  </Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={LoginPage} />
+          <Route path="/main" component={MainPage} />
+          <Route path="/welcome" component={WelcomePage} />
+          <Route path="/signup" component={ModalPage} />
+          <Route path="/mypage" component={MyPage} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
-import { setRecords, axiosData } from "../actions/recordAction";
-import { useDispatch } from "react-redux";
 import axios from "axios";
+import "../componentsCss/UserInfo.css";
 
 function UserInfo() {
   const userInfo = useSelector((state) => state.recordReducer).user;
   const { email, userName, tags } = userInfo;
   const [password, setPassword] = useState("");
   const [chkPassword, setChkPassword] = useState("");
+  const [grade, setGrade] = useState(false);
 
   const passwordHandler = (e) => {
     setPassword(e.target.value);
@@ -17,14 +17,10 @@ function UserInfo() {
   const chkPasswordHandler = (e) => {
     setChkPassword(e.target.value);
   };
-  console.log(password);
-  console.log(chkPassword);
 
   const newPassword = { newPassword: password };
 
   const validationPassword = async () => {
-    // console.log(password);
-    // console.log(chkPassword);
     if (password.length < 8) alert("8자 이상 20자 이하로 입력해주세요");
     else if (password.length > 20) alert("8자 이상 20자 이하로 입력해주세요");
     else if (password !== chkPassword) alert("비밀번호가 일치하지 않습니다");
@@ -33,42 +29,71 @@ function UserInfo() {
         .post("https://localhost:5000/user/userinfo", newPassword, { accept: "application/json", withCredentials: true })
         .then((res) => {
           console.log(res);
-          if (res.data.message === "Your password has been changed successfully.") {
+          if (res.data.message === "Your password has been changed successfully") {
             setPassword("");
             setChkPassword("");
-          } else if (res.data.message === "Same password as before") {
-            alert("기존 비밀번호와 달라야 합니다");
-          } else {
-            alert("세션이 만료되었습니다. 다시 로그인 해주세요");
+            alert("성공적으로 변경 완료 되었습니다");
           }
         })
         .catch((e) => {
           // ! 에러 메세지 확인
-          //  else if (res.data.message === "Same password as before") {
-          //   alert("기존 비밀번호와 달라야 합니다");
-          // } else {
-          //   alert("세션이 만료되었습니다. 다시 로그인 해주세요");
-          // }
-          console.log(e);
+          if (e.message === "Request failed with status code 403") {
+            alert("기존 비밀번호와 달라야 합니다");
+          } else {
+            alert("세션이 만료되었습니다. 다시 로그인 해주세요");
+          }
+          console.log(e.message);
         });
     }
   };
 
+  const chkGradeList = () => {
+    setGrade(grade === false ? true : false);
+  };
+  const style = {
+    border: "2px solid red",
+  };
+
+  const gradeStyle = {
+    color: "gray",
+  };
+
+  const allGrade = ["비기너 : 0시간", "아이언 : 5시간", "브론즈 : 10시간", "실버 : 25시간", "골드 : 50시간", "플레티넘 : 100시간", "다이아 : 150시간", "마스터 : 200시간", "그랜드마스터 : 300시간", "챌린져 : 500시간"];
+
   return (
-    <div>
+    <div className="userInfo">
       <div>
         <div>name : {userName}</div>
         <div>email : {email}</div>
         <div>
-          <span>칭호 : {tags.map((x) => x)} (모달)</span>
-          <button>확인</button>
-        </div>
-        <div>
           <span>비밀번호</span>
-          <input type="password" minlength="8" maxLength="20" onChange={passwordHandler} autoComplete="new-password" autocomplete="false" />
-          <input type="password" minLength="8" maxLength="20" onChange={chkPasswordHandler} autoComplete="off" />
+          <input type="password" minLength="8" maxLength="20" value={password} onChange={passwordHandler} />
+          <input type="password" minLength="8" maxLength="20" value={chkPassword} onChange={chkPasswordHandler} />
           <button onClick={validationPassword}>수정</button>
         </div>
+        <div>
+          <span onClick={chkGradeList} style={{ cursor: "pointer" }}>
+            칭호 : {tags[tags.length - 1]}
+          </span>
+          <button onClick={chkGradeList}>확인을 누르면 칭호가 튀어나옵니다</button>
+        </div>
+        {grade && (
+          <div>
+            <div style={style}>
+              <div>
+                {allGrade.reverse().map((x, idx) =>
+                  tags.includes(x.split(" ")[0]) ? (
+                    <div key={idx}>{x}</div>
+                  ) : (
+                    <div key={idx} style={gradeStyle}>
+                      {x}
+                    </div>
+                  )
+                )}
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
