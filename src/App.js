9ab7c61297e38113d6a81077df2f3a89904bb381
sangrae-cho <tkdfo93@gamil.com>
2021-03-23 11:47:11 +0900

    Feat#74: add loding page of oauth system

--- src/App.js
+++ src/App.js
@@ -13,16 +13,19 @@ import LoginPage from './pages/LoginPage'
 import SignupPage from './pages/SignupPage'
 import MyPage from './pages/MyPage'
 import WelcomePage from './pages/WelcomePage'
+import Loading from './pages/Loading'
 import {useSelector} from 'react-redux'
 
  
 
 function App() {
   const dispatch = useDispatch()
-  const [accessToken,setAccessToken] = useState('') 
+  const [accessToken,setAccessToken] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
 
   const getAccessToken =(authorizationCode)=>{
+    setIsLoading(true);
     axios.post('https://localhost:5000/main/oauth/accesstoken', { authorizationCode: authorizationCode,accept:'application/json',withCredentials:true})
     .then(res=> {
       setAccessToken(res.data.data.accessToken)})
@@ -39,9 +42,10 @@ function App() {
         .then(res => {
           dispatch(setIsLogin());
           dispatch(setUserInfo());
-        })
+        }).then(d => {setIsLoading(false)})
       })
-
+    }).catch(e => {
+      alert('OAuth 요청에 실패하였습니다.');
     })
   }
 
@@ -59,11 +63,11 @@ function App() {
 
 
   useEffect(()=>{
-    
   },[useSelector(s=>s.recordReducer.isLogin)])
 
 
   return (
+    isLoading ? <Loading /> :
     <Router>
     <div>
       <Switch>
