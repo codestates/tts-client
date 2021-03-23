cab9b4c59acddbe5b6b5e05e4acce29f45472651
ohveloper <ohveloper@gmail.com>
2021-03-23 15:31:33 +0900

    Feat#20 : -ing

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
 function UserInfo() {
-    return (
+  const userInfo = useSelector((state) => state.recordReducer).user;
+  const { email, userName, tags } = userInfo;
+  return (
+    <div>
+      <div>
+        <div>name : {userName}</div>
+        <div>email : {email}</div>
         <div>
-            helloe
+          <span>칭호 : {tags.map((x) => x)} (모달)</span>
+          <button>확인</button>
         </div>
-    )
+        <div>
+          <span>비밀번호</span>
+          <button>수정</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default UserInfo
+export default UserInfo;
--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,27 +1,17 @@
 import React from "react";
-import NavBar from "../components/NavBar";
 import { useSelector } from "react-redux";
+import NavBar from "../components/NavBar";
 import GuestNavBar from "../components/GuestNavbar";
+import UserInfo from "../components/UserInfo";
 
 function MyPage() {
   const x = useSelector((s) => s.recordReducer.isLogin);
-  const userInfo = useSelector((state) => state.recordReducer).user;
-  const { email, userName, tags } = userInfo;
 
   return (
     <div>
       <div>{x ? <NavBar /> : <GuestNavBar />}</div>
       <div>
-        <div>name : {userName}</div>
-        <div>email : {email}</div>
-        <div>
-          <span>칭호 : {tags.map((x) => x)}(모달)</span>
-          <button>확인</button>
-        </div>
-        <div>
-          <span>비밀번호</span>
-          <button>수정</button>
-        </div>
+        <UserInfo />
       </div>
     </div>
   );
