48e2f9adc8e09a3d48bd03f542cbd6881e71220c
reo-dev <dbckd22@gmail.com>
2021-03-23 11:09:54 +0900

    feat#18: done

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,19 @@ import { compose, createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/index';
 import thunk from 'redux-thunk';
 import promiseMiddleware from 'redux-promise';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
 
+const persistConfig = {
+  key: 'root',
+  storage
+};
+
+const enhancedReducer = persistReducer(persistConfig, rootReducer);
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
   ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
   : compose;
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk,promiseMiddleware)));
+const store = createStore(enhancedReducer, composeEnhancers(applyMiddleware(thunk,promiseMiddleware)));
 
 export default store;
